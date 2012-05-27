ef8a94caf206d844ab41913734065fb438d6329f
George Birbilis <birbilis@users.noreply.github.com>
2012-05-27 08:09:00 +0300

    ClipFlair client projects

---
+++ Client/ClipFlair/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ClipFlair
+{
+  public partial class MainPage : UserControl
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+   
+    private void Page_MouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      double scalingFactor = (double)e.Delta / 110;
+      if (scalingFactor > 0)
+      {
+        viewbox.Width *= scalingFactor;
+        viewbox.Height *= scalingFactor;
+      }
+      else
+      {
+        viewbox.Width /= -scalingFactor;
+        viewbox.Height /= -scalingFactor;
+      }
+    }
+  }
+}
---
+++ Client/PropertyPagesSupport/Models/IPropertyPagesHost.cs
@@ -0,0 +1,11 @@
+﻿//Version: 20120428
+
+namespace PropertyPagesSupport
+{
+
+  public interface IPropertyPagesHost: IPropertyPagesConsumer, IPropertyPagesProvider
+  {
+ 
+  }
+
+}
