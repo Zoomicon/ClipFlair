6a61edc16257a4a712eb1a679b16c3e4cedf72a2
George Birbilis <birbilis@users.noreply.github.com>
2012-06-11 03:00:47 +0300

    #1224 - FlipPanel control

---
+++ Client/FlipPanel/FlipPanel.Silverlight.Demo/FlipPanelAlternateTemplate.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace FlipPanelTest
+{
+    public partial class FlipPanelAlternateTemplate : UserControl
+    {
+        public FlipPanelAlternateTemplate()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdFlip_Click(object sender, RoutedEventArgs e)
+        {
+            panel.IsFlipped = !panel.IsFlipped;
+        }
+    }
+}
---
+++ Client/FlipPanel/FlipPanel.Silverlight.Demo/FlipPanelTest.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+//using FlipPanel;
+
+namespace FlipPanelTest
+{
+    public partial class FlipPanelTest : UserControl
+    {
+        public FlipPanelTest()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdFlip_Click(object sender, RoutedEventArgs e)
+        {            
+            panel.IsFlipped = !panel.IsFlipped;
+        }
+    }
+}
