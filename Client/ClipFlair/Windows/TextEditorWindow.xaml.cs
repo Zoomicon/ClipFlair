5cb91ad9819dfb112c6fcabdf5548f56e273bb5c
Birbilis <Birbilis@localhost>
2012-06-26 23:58:45 +0300

    #1234 SilverlightTextEditor renamed to SilverTextEditor to cater for possible WPF port

---
+++ Client/ClipFlair/Windows/TextEditorWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SilverFlow.Controls;
+
+namespace ClipFlair
+{
+    public partial class FloatingWindow1 : FloatingWindow
+    {
+        public FloatingWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
