5f539f11a63f75e24e2846a638caf203c3f486b4
Birbilis <Birbilis@localhost>
2012-07-02 14:42:41 +0300

    #1220 - ZoomAndPan demos

---
+++ Client/ZoomAndPan/ZoomAndPan.Demo.TestZoom/ZoomAndPan.Demo.TestZoom.Silverlight/Controls/MouseHandlingMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TestZoom.Controls
+{
+
+    /// <summary>
+    /// Defines the current state of the mouse handling logic.
+    /// </summary>
+    public enum MouseHandlingMode
+    {
+
+        None,
+        DraggingRectangles,
+        Panning,
+        Zooming,
+        DragZooming,
+    }
+}
