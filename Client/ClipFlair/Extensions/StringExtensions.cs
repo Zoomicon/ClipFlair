c6bcafd1e787ef1e1811b7cb361cf8e625f50252
Birbilis <Birbilis@localhost>
2012-07-02 14:46:29 +0300

    #1238 - ClipFlair activity container: MediaPlayerWindow shows streaming video sample and sample property page

---
+++ Client/ClipFlair/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿//Version: 20120702
+
+using System;
+
+namespace ClipFlair.Extensions
+{
+    public static class StringExtensions
+    {
+
+        public static Uri ToUri(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                return null;
+            else
+                return new Uri(input, UriKind.Absolute);
+        }
+
+    }
+}
