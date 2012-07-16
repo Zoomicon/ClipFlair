743cec47f4ab7ab702e5c9d2f540fd00388be510
Administrator <Administrator@clipflairsrv.cti.gr>
2012-07-16 15:44:02 +0300

    #1238 - added Converters project (for DataBinding)

---
+++ Client/Converters/VisibilityToBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿//Version: 201207122
+#if SILVERLIGHT
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Converters
+{
+
+  [ValueConversion(typeof(Visibility), typeof(bool))]
+  public class VisibilityToBooleanConverter : ReverseConverter
+  {
+    
+    public VisibilityToBooleanConverter() : base(new BooleanToVisibilityConverter())
+    {
+
+    }
+
+  }
+
+}
+
+#endif
