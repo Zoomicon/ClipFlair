77510ed237ff5e47db9dafb499e9738632e0788e
George <George@BlackPC>
2012-08-01 14:26:39 +0300

    #1238 ClipFlair app: using Views for the windows/components

---
+++ Client/ClipFlair/Views/BaseView.cs
@@ -0,0 +1,26 @@
+﻿//Filename: BaseView.cs
+//Version: 20120730
+
+using ClipFlair.Models.Views;
+
+using System.ComponentModel;
+
+namespace ClipFlair.Views
+{
+  public class BaseView: IView
+  {
+    #region INotifyPropertyChanged
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void RaisePropertyChanged(string PropertyName)
+    {
+      if (PropertyChanged != null)
+        PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+    }
+
+    #endregion
+
+  }
+
+}
---
+++ Client/ClipFlair/Views/MediaPlayerView.cs
@@ -0,0 +1,113 @@
+﻿//Filename: MediaPlayerView.cs
+//Version: 20120730
+
+using ClipFlair.Models.Views;
+
+using System;
+
+namespace ClipFlair.Views
+{
+  public class MediaPlayerView: BaseView, IMediaPlayer
+  {
+    public MediaPlayerView()
+    {
+      Time = IMediaPlayerDefaults.DefaultTime;
+      Speed = IMediaPlayerDefaults.DefaultSpeed;
+      Volume = IMediaPlayerDefaults.DefaultVolume;
+      ControllerVisible = IMediaPlayerDefaults.DefaultControllerVisible;
+      CaptionsVisible = IMediaPlayerDefaults.DefaultCaptionsVisible;
+    }
+        
+    #region IMediaPlayer
+
+    private TimeSpan time;
+    private double speed;
+    private double volume;
+    private bool controllerVisible;
+    private bool captionsVisible;
+
+    public TimeSpan Time
+    {
+      get { return time; }
+      set
+      {
+        if (value != time)
+        {
+          time = value;
+          RaisePropertyChanged(IMediaPlayerProperties.PropertyTime);
+        }
+      }
+    }
+
+    public double Speed
+    {
+      get { return speed; }
+      set
+      {
+        if (value != speed)
+        {
+          speed = value;
+          RaisePropertyChanged(IMediaPlayerProperties.PropertySpeed);
+        }
+      }
+    }
+
+    public double Volume
+    {
+      get { return volume; }
+      set
+      {
+        if (value != volume)
+        {
+          volume = value;
+          RaisePropertyChanged(IMediaPlayerProperties.PropertyVolume);
+        }
+      }
+    }
+
+    public bool ControllerVisible
+    {
+      get { return controllerVisible; }
+      set
+      {
+        if (value != controllerVisible)
+        {
+          controllerVisible = value;
+          RaisePropertyChanged(IMediaPlayerProperties.PropertyControllerVisible);
+        }
+      }
+    }
+
+    public bool CaptionsVisible
+    {
+      get { return captionsVisible; }
+      set
+      {
+        if (value != captionsVisible)
+        {
+          captionsVisible = value;
+          RaisePropertyChanged(IMediaPlayerProperties.PropertyCaptionsVisible);
+        }
+      }
+    }
+
+    public void Play()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Pause()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+      Pause();
+      Time = TimeSpan.Zero;
+    }
+
+    #endregion
+  }
+
+}
