770540a5e6b18dffddf1f1f0b7307dbc1e621d92
birbilis <birbilis@localhost>
2012-08-23 22:44:17 +0300

    #1238 ClipFlair: added ToolbarVisible property to ITextEditor model

--- Models/ClipFlairModels/Views/ITextEditor.cs
+++ Models/ClipFlairModels/Views/ITextEditor.cs
@@ -1,5 +1,5 @@
 ﻿//Filename: ITextEditor.cs
-//Version: 20120814
+//Version: 20120823
 
 using System;
 
@@ -9,16 +9,19 @@ namespace ClipFlair.Models.Views
   public static class ITextEditorProperties
   {
     public const string PropertySource = "Source";
+    public const string PropertyToolbarVisible = "ToolbarVisible";
   }
 
   public static class ITextEditorDefaults
   {
     public static Uri DefaultSource = null;
+    public static bool DefaultToolbarVisible = true;
   }
   
   public interface ITextEditor : IView
   {
     Uri Source { get; set; }
+    bool ToolbarVisible { get; set; }
   }
 
 }
