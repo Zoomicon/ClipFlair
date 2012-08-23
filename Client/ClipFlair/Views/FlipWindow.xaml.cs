3ab28dd7a9f161a07df2368ff85a1b96066e3fad
birbilis <birbilis@localhost>
2012-08-24 00:56:33 +0300

    #1238 ClipFlair: FlipWindow (base window used) now sets CacheMode if GPU acceleration is on

--- Client/ClipFlair/Views/FlipWindow.xaml.cs
+++ Client/ClipFlair/Views/FlipWindow.xaml.cs
@@ -1,44 +1,51 @@
 ﻿//Filename: FlipWindow.xaml.cs
-//Version: 20120814
+//Version: 20120823
+
+using SilverFlow.Controls;
 
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
-using SilverFlow.Controls;
-                                        
+using System.Windows.Media;
+
 namespace ClipFlair.Components
 {
-    [ContentProperty("FrontContent")]
-    public partial class FlipWindow : FloatingWindow
+  [ContentProperty("FrontContent")]
+  public partial class FlipWindow : FloatingWindow
+  {
+    public FlipWindow()
     {
-        public FlipWindow()
-        {
-            InitializeComponent();
-            ShowMaximizeButton = false; //!!! (till we fix it to resize to current visible view area and to allow moving the window in that case only [when it's not same size as parent])
-        }
+      if (Application.Current.Host.Settings.EnableGPUAcceleration) //GPU acceleration can been turned on at HTML/ASPX page or at OOB settings for OOB apps
+        CacheMode = new BitmapCache(); //must do this before setting the "Scale" property, since it will set "RenderAtScale" property of the BitmapCache
 
-        public object FrontContent
-        {
-            get { return FlipPanel.FrontContent; }
-            set { FlipPanel.FrontContent = value; }
-        }
+      ShowMaximizeButton = false; //!!! (till we fix it to resize to current visible view area and to allow moving the window in that case only [when it's not same size as parent])
 
-        public object BackContent //if one wants to replace the default backcontent that hosts the PropertiesPanel etc.
-        {
-          get { return FlipPanel.BackContent; }
-          set { FlipPanel.BackContent = value; }
-        }
-
-        public UIElementCollection PropertyItems
-        {
-            get { return PropertiesPanel.Children; }
-            set { 
-              PropertiesPanel.Children.Clear();
-              foreach (UIElement item in value) { PropertiesPanel.Children.Add(item); }
-            }
-        }
-
-
+      InitializeComponent(); //this can override the "ShowMaximize" button, or set the "Scale" property, so must do last
     }
+
+    public object FrontContent
+    {
+      get { return FlipPanel.FrontContent; }
+      set { FlipPanel.FrontContent = value; }
+    }
+
+    public object BackContent //if one wants to replace the default backcontent that hosts the PropertiesPanel etc.
+    {
+      get { return FlipPanel.BackContent; }
+      set { FlipPanel.BackContent = value; }
+    }
+
+    public UIElementCollection PropertyItems
+    {
+      get { return PropertiesPanel.Children; }
+      set
+      {
+        PropertiesPanel.Children.Clear();
+        foreach (UIElement item in value) { PropertiesPanel.Children.Add(item); }
+      }
+    }
+
+
+  }
 
 }
