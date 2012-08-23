8069966d900112c1d2b553f3cff90b59379338ae
birbilis <birbilis@localhost>
2012-08-23 22:51:27 +0300

    #1238 ClipFlair: enabled GPU acceleration at OOB mode, added ToolbarVisible to TextEditorView and TextEditorWindow, changed dependency properties to not use hardcoded property names and default values

--- Client/ClipFlair/Views/MediaPlayerWindow.xaml.cs
+++ Client/ClipFlair/Views/MediaPlayerWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿//Filename: MediaPlayerWindow.xaml.cs
-//Version: 20120819
+//Version: 20120823
 
 using ClipFlair.Views;
+using ClipFlair.Models.Views;
 
 using Microsoft.SilverlightMediaFramework;
 using Microsoft.SilverlightMediaFramework.Core.Media;
@@ -16,99 +17,100 @@ using System.ComponentModel;
 
 namespace ClipFlair.Components
 {
-    public partial class MediaPlayerWindow : FlipWindow
+  public partial class MediaPlayerWindow : FlipWindow
+  {
+    public MediaPlayerWindow()
     {
-        public MediaPlayerWindow()
-        {
-          View = new MediaPlayerView(); //must set the view first
-          InitializeComponent();
-        }
-
-        #region View
-
-        public MediaPlayerView View
-        {
-          get { return (MediaPlayerView)DataContext; }
-          set {
-            //remove property changed handler from old view
-            if (DataContext!=null)
-              ((INotifyPropertyChanged)DataContext).PropertyChanged -= new PropertyChangedEventHandler(View_PropertyChanged);
-            //add property changed handler to new view
-            value.PropertyChanged += new PropertyChangedEventHandler(View_PropertyChanged);
-            //set the new view
-            DataContext = value;
-          }
-        }
-
-        protected void View_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-          if (e.PropertyName.Equals("Source"))
-          {
-            Source = View.Source;
-            UpdatePlaylist();
-          }
-        }
-
-        #endregion
-
-        #region Source
-
-        /// <summary>
-        /// Source Dependency Property
-        /// </summary>
-        public static readonly DependencyProperty SourceProperty =
-            DependencyProperty.Register("Source", typeof(Uri), typeof(MediaPlayerWindow),
-                new FrameworkPropertyMetadata((Uri)null, new PropertyChangedCallback(OnSourceChanged)));
-
-        /// <summary>
-        /// Gets or sets the Source property.
-        /// </summary>
-        public Uri Source
-        {
-          get { return (Uri)GetValue(SourceProperty); }
-          set { SetValue(SourceProperty, value); }
-        }
-
-        /// <summary>
-        /// Handles changes to the Source property.
-        /// </summary>
-        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-          MediaPlayerWindow target = (MediaPlayerWindow)d;
-          target.OnSourceChanged((Uri)e.OldValue, target.Source);
-        }
-
-        /// <summary>
-        /// Provides derived classes an opportunity to handle changes to the IsAvailable property.
-        /// </summary>
-        protected virtual void OnSourceChanged(Uri oldSource, Uri newSource)
-        {
-          View.Source = newSource;
-        }
-
-        #endregion
-
-      #region Playlist
-
-      public void UpdatePlaylist()
-      {
-        PlaylistItem playlistItem = new PlaylistItem();
-        
-        playlistItem.MediaSource = View.Source;
-        playlistItem.DeliveryMethod = Microsoft.SilverlightMediaFramework.Plugins.Primitives.DeliveryMethods.AdaptiveStreaming; //TODO: need to set that: could decide based on the URL format (e.g. if ends at .ism/Manifest is adaptive streaming)
-        
-        List<MarkerResource> markerResources = new List<MarkerResource>();
-        MarkerResource markerResource = new MarkerResource();
-        markerResource.Source = new Uri("ExampleCaptions.xml", UriKind.Relative); //TODO: change
-        markerResources.Add(markerResource);
-        playlistItem.MarkerResources = markerResources;
-
-        //player.Playlist.Clear(); //skip this to allow going back to previous items from playlist
-        player.Playlist.Add(playlistItem);
-        player.GoToPlaylistItem(player.Playlist.Count-1);
-      }
-      
-      #endregion
-
+      View = new MediaPlayerView(); //must set the view first
+      InitializeComponent();
     }
+
+    #region View
+
+    public MediaPlayerView View
+    {
+      get { return (MediaPlayerView)DataContext; }
+      set
+      {
+        //remove property changed handler from old view
+        if (DataContext != null)
+          ((INotifyPropertyChanged)DataContext).PropertyChanged -= new PropertyChangedEventHandler(View_PropertyChanged);
+        //add property changed handler to new view
+        value.PropertyChanged += new PropertyChangedEventHandler(View_PropertyChanged);
+        //set the new view
+        DataContext = value;
+      }
+    }
+
+    protected void View_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      if (e.PropertyName.Equals(IMediaPlayerProperties.PropertySource))
+      {
+        Source = View.Source;
+        UpdatePlaylist();
+      }
+    }
+
+    #endregion
+
+    #region Source
+
+    /// <summary>
+    /// Source Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register(IMediaPlayerProperties.PropertySource, typeof(Uri), typeof(MediaPlayerWindow),
+            new FrameworkPropertyMetadata((Uri)IMediaPlayerDefaults.DefaultSource, new PropertyChangedCallback(OnSourceChanged)));
+
+    /// <summary>
+    /// Gets or sets the Source property.
+    /// </summary>
+    public Uri Source
+    {
+      get { return (Uri)GetValue(SourceProperty); }
+      set { SetValue(SourceProperty, value); }
+    }
+
+    /// <summary>
+    /// Handles changes to the Source property.
+    /// </summary>
+    private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      MediaPlayerWindow target = (MediaPlayerWindow)d;
+      target.OnSourceChanged((Uri)e.OldValue, target.Source);
+    }
+
+    /// <summary>
+    /// Provides derived classes an opportunity to handle changes to the IsAvailable property.
+    /// </summary>
+    protected virtual void OnSourceChanged(Uri oldSource, Uri newSource)
+    {
+      View.Source = newSource;
+    }
+
+    #endregion
+
+    #region Playlist
+
+    public void UpdatePlaylist()
+    {
+      PlaylistItem playlistItem = new PlaylistItem();
+
+      playlistItem.MediaSource = View.Source;
+      playlistItem.DeliveryMethod = Microsoft.SilverlightMediaFramework.Plugins.Primitives.DeliveryMethods.AdaptiveStreaming; //TODO: need to set that: could decide based on the URL format (e.g. if ends at .ism/Manifest is adaptive streaming)
+
+      List<MarkerResource> markerResources = new List<MarkerResource>();
+      MarkerResource markerResource = new MarkerResource();
+      markerResource.Source = new Uri("ExampleCaptions.xml", UriKind.Relative); //TODO: change
+      markerResources.Add(markerResource);
+      playlistItem.MarkerResources = markerResources;
+
+      //player.Playlist.Clear(); //skip this to allow going back to previous items from playlist
+      player.Playlist.Add(playlistItem);
+      player.GoToPlaylistItem(player.Playlist.Count - 1);
+    }
+
+    #endregion
+
+  }
 }
