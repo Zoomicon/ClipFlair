adbc89e31cad3eee9c297e82cce6296dcab82769
George Birbilis <birbilis@users.noreply.github.com>
2012-09-02 02:25:42 +0300

    #1246: MediaPlayer: added MediaMarkerWrapper<T> with DependencyProperties for Begin/End/Duration/Content so that we can bind to wrapped MediaMarkers added MediaMarkerCollectionWrapper<T> to wrap MediaMarkerCollection<T> and expose each MediaMarker<T> in it via MediaMarkerWrapper<T> wrappers

---
+++ Client/MediaPlayer/MediaPlayer.Silverlight/MediaMarkerCollectionWrapper.cs
@@ -0,0 +1,46 @@
+﻿//Filename: MediaMarkerWrapper.cs
+//Version: 20120902
+
+using System;
+using System.Windows;
+using System.ComponentModel;
+
+using Microsoft.SilverlightMediaFramework.Core.Media;
+using Microsoft.SilverlightMediaFramework.Plugins.Primitives;
+using Microsoft.SilverlightMediaFramework.Utilities;
+
+namespace Zoomicon.MediaPlayer
+{
+ 
+  public class MediaMarkerCollectionWrapper<T> : ScriptableObservableCollection<MediaMarkerWrapper<T>> where T : MediaMarker
+  {
+
+    #region --- Fields ---
+
+    private MediaMarkerCollection<T> markers;
+
+    #endregion
+
+    #region --- Properties ---
+
+    public MediaMarkerCollection<T> Markers
+    {
+      get { return null; }
+      set
+      { //TODO: listen for collection changed events (see ObservableCollection doc) on the wrapped list and on our list and sync them (using MediaMarkerWrapper objects for each item)
+        //remove property changed handler from old media marker
+        if (markers != null)
+          ;// ((INotifyPropertyChanged)markers).PropertyChanged -= new PropertyChangedEventHandler(Markers_PropertyChanged);
+        //set the new media marker
+        markers = value;
+        //add property changed handler to new media marker
+        if (markers != null)
+          ;// ((INotifyPropertyChanged)markers).PropertyChanged += new PropertyChangedEventHandler(Markers_PropertyChanged);
+      }
+    }
+  
+    #endregion
+
+  }
+
+}
