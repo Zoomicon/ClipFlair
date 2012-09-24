fa313855ebbfb0b738bf3dcd33df63134f394911
George Birbilis <birbilis@users.noreply.github.com>
2012-09-24 16:19:26 +0300

    #1238 ClipFlair: fixed issue with scrollbars not showing up when needed, added ActivityView and related model

--- Client/ClipFlair/Views/ActivityContainer.xaml.cs
+++ Client/ClipFlair/Views/ActivityContainer.xaml.cs
@@ -1,8 +1,11 @@
 ﻿//Filename: ActivityContainer.xaml.cs
-//Version: 20120904
+//Version: 20120923
 
-//using SilverFlow.Controls;
+using ClipFlair.Models.Views;
 
+using System;
+using System.Windows;
+using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Data;
 
@@ -12,14 +15,16 @@ namespace ClipFlair.Views
   {
     public ActivityContainer()
     {
+      View = new ActivityView(); //must set the view first
       InitializeComponent();
 
+      //(assuming Using SilverFlow.Controls)
       //zuiContainer.Add((FloatingWindow)XamlReader.Load("<clipflair:MediaPlayerWindow Width=\"Auto\" Height=\"Auto\" MinWidth=\"100\" MinHeight=\"100\" Title=\"4 Media Player\" IconText=\"1Media Player\" Tag=\"1MediaPlayer\" xmlns:clipflair=\"clr-namespace:ClipFlair;assembly=ClipFlair\"/>"));
       //zuiContainer.Add(new MediaPlayerWindow()).Show();
-
       //zuiContainer.FloatingWindows.First<FloatingWindow>().Show();
 
       BindWindows(); //Bind MediaPlayerWindows's Time to CaptionGridWindows's Time programmatically (since using x:Name on the controls doesn't seem to work [components are resolved to null when searched by name - must have to do with the implementation of the Windows property of ZUI container])
+      //above command could be avoided if binding to activity view's Time property worked
     }
 
     private void BindWindows()
@@ -36,6 +41,126 @@ namespace ClipFlair.Views
         if (tag == (string)w.Tag) return w; //must cast to string to compare (else we compare object references, since Tag property is of type object, not string)
       return null;
     }
+
+    #region View
+
+    public ActivityView View
+    {
+      get { return (ActivityView)DataContext; }
+      set
+      {
+        //remove property changed handler from old view
+        if (DataContext != null)
+          ((INotifyPropertyChanged)DataContext).PropertyChanged -= new PropertyChangedEventHandler(View_PropertyChanged);
+        //add property changed handler to new view
+        if (value != null)
+          value.PropertyChanged += new PropertyChangedEventHandler(View_PropertyChanged);
+        //set the new view (must do last)
+        DataContext = value;
+      }
+    }
+
+    protected void View_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      if (e.PropertyName == null) //multiple (not specified) properties have changed, consider all as changed
+      {
+        Source = View.Source;
+        Time = View.Time;
+        //...
+      }
+      else switch (e.PropertyName) //string equality check in .NET uses ordinal (binary) comparison semantics by default
+        {
+          case IActivityProperties.PropertySource:
+            Source = View.Source;
+            break;
+          case IActivityProperties.PropertyTime:
+            Time = View.Time;
+            break;
+          //...
+        }
+    }
+
+    #endregion
+
+    #region Source
+
+    /// <summary>
+    /// Source Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty SourceProperty =
+        DependencyProperty.Register(IActivityProperties.PropertySource, typeof(Uri), typeof(ActivityContainer),
+            new FrameworkPropertyMetadata(IActivityDefaults.DefaultSource, new PropertyChangedCallback(OnSourceChanged)));
+
+    /// <summary>
+    /// Gets or sets the Source property.
+    /// </summary>
+    public Uri Source
+    {
+      get { return (Uri)GetValue(SourceProperty); }
+      set { SetValue(SourceProperty, value); }
+    }
+
+    /// <summary>
+    /// Handles changes to the Source property.
+    /// </summary>
+    private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      ActivityContainer target = (ActivityContainer)d;
+      target.OnSourceChanged((Uri)e.OldValue, target.Source);
+    }
+
+    /// <summary>
+    /// Provides derived classes an opportunity to handle changes to the Source property.
+    /// </summary>
+    protected virtual void OnSourceChanged(Uri oldSource, Uri newSource)
+    {
+      View.Source = newSource;
+      //...
+    }
+
+    #endregion
+
+    #region Time
+
+    /// <summary>
+    /// Time Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty TimeProperty =
+        DependencyProperty.Register(IActivityProperties.PropertyTime, typeof(TimeSpan), typeof(ActivityContainer),
+            new FrameworkPropertyMetadata(IActivityDefaults.DefaultTime,
+                FrameworkPropertyMetadataOptions.None,
+                new PropertyChangedCallback(OnTimeChanged)));
+
+    /// <summary>
+    /// Gets or sets the Time property.
+    /// </summary>
+    public TimeSpan Time
+    {
+      get { return (TimeSpan)GetValue(TimeProperty); }
+      set { SetValue(TimeProperty, value); }
+    }
+
+    /// <summary>
+    /// Handles changes to the Time property.
+    /// </summary>
+    private static void OnTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      ActivityContainer target = (ActivityContainer)d;
+      TimeSpan oldTime = (TimeSpan)e.OldValue;
+      TimeSpan newTime = target.Time;
+      target.OnTimeChanged(oldTime, newTime);
+    }
+
+    /// <summary>
+    /// Provides derived classes an opportunity to handle changes to the Time property.
+    /// </summary>
+    protected virtual void OnTimeChanged(TimeSpan oldTime, TimeSpan newTime)
+    {
+      View.Time = newTime;
+      //...
+    }
+
+    #endregion
 
   }
   
---
+++ Client/ClipFlair/Views/ViewModels/ActivityView.cs
@@ -0,0 +1,64 @@
+﻿//Filename: ActivityView.cs
+//Version: 20120923
+
+using ClipFlair.Models.Views;
+
+using System;
+using System.Runtime.Serialization;
+
+namespace ClipFlair.Views
+{
+
+  [DataContract(Namespace = "http://clipflair.net/Contracts/Views")]
+  public class ActivityView: BaseView, IActivity
+  {
+    public ActivityView()
+    {
+    }
+        
+    #region IActivity
+
+    #region Fields
+
+    //can set fields directly here or at constructor
+    private Uri source = IActivityDefaults.DefaultSource;
+    private TimeSpan time = IActivityDefaults.DefaultTime;
+
+    #endregion
+
+    #region Properties
+    
+    [DataMember]
+    public Uri Source
+    {
+      get { return source; }
+      set
+      {
+        if (value != source)
+        {
+          source = value;
+          RaisePropertyChanged(IActivityProperties.PropertySource);
+        }
+      }
+    }
+
+    [DataMember]
+    public TimeSpan Time
+    {
+      get { return time; }
+      set
+      {
+        if (value != time)
+        {
+          time = value;
+          RaisePropertyChanged(IActivityProperties.PropertyTime);
+        }
+      }
+    }
+
+    #endregion
+
+    #endregion
+  }
+
+}
