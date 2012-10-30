ce0bb995bdbc9e1158dfada9e2ca0939d6cb3a7f
birbilis <birbilis@localhost>
2012-10-30 21:08:29 +0200

    #1238 ClipFlair: added Scale property to BaseWindow's View and back panel, binding components' Time property to ActivityContainer as a means to easily bind them all together

--- Models/ClipFlairModels/Views/IView.cs
+++ Models/ClipFlairModels/Views/IView.cs
@@ -1,6 +1,6 @@
 ﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
 //Filename: IView.cs
-//Version: 20121003
+//Version: 20121030
 
 using System.ComponentModel;
 using System.Windows;
@@ -12,6 +12,7 @@ namespace ClipFlair.Models.Views
   {
     public const string PropertyTitle = "Title";
     public const string PropertyPosition = "Position";
+    public const string PropertyScale = "Scale";
     public const string PropertyWidth = "Width";
     public const string PropertyHeight = "Height";
   }
@@ -20,6 +21,7 @@ namespace ClipFlair.Models.Views
   {
     public static string DefaultTitle = "";
     public static Point DefaultPosition = new Point(0, 0);
+    public static double DefaultScale = 1.0;
     public static double DefaultWidth = 0;
     public static double DefaultHeight = 0;
   }
@@ -28,6 +30,7 @@ namespace ClipFlair.Models.Views
   {
     string Title { get; set; }
     Point Position { get; set; }
+    double Scale { get; set; }
     double Width { get; set; }
     double Height { get; set; }
   }
