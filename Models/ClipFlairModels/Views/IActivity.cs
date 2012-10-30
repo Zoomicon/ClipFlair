2aba2fe5f3bd1152066f1121313144e798963e1d
birbilis <birbilis@localhost>
2012-10-30 19:01:30 +0200

    #1238 ClipFlair: added Offset and Scale properties to IActivity

--- Models/ClipFlairModels/Views/IActivity.cs
+++ Models/ClipFlairModels/Views/IActivity.cs
@@ -1,8 +1,9 @@
 ﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
 //Filename: IActivity.cs
-//Version: 20121004
+//Version: 20121030
 
 using System;
+using System.Windows;
 
 namespace ClipFlair.Models.Views
 {
@@ -11,18 +12,27 @@ namespace ClipFlair.Models.Views
   {
     public const string PropertySource = "Source";
     public const string PropertyTime = "Time";
+
+    public const String PropertyOffset = "Offset";
+    public const String PropertyScale = "Scale";
   }
 
   public static class IActivityDefaults
   {
     public static Uri DefaultSource = null;
     public static TimeSpan DefaultTime = TimeSpan.Zero;
+
+    public static Point DefaultOffset = new Point(0, 0);
+    public static double DefaultScale = 1.0;
   }
   
   public interface IActivity: IView
   {
     Uri Source { get; set; }
     TimeSpan Time { get; set; }
+
+    Point Offset { get; set; }
+    double Scale { get; set; }
   }
 
 }
