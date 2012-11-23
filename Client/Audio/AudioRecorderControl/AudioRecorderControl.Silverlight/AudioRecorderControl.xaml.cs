337ecba4b9eb56c8e124f986d30b295dc37590c9
George Birbilis <birbilis@users.noreply.github.com>
2012-11-24 01:17:58 +0200

    #1241 - AudioRecorderControl: moved most functionality into the view

--- Client/Audio/AudioRecorderControl/AudioRecorderControl.Silverlight/AudioRecorderControl.xaml.cs
+++ Client/Audio/AudioRecorderControl/AudioRecorderControl.Silverlight/AudioRecorderControl.xaml.cs
@@ -1,55 +1,44 @@
-﻿//Filename: AudioRecorderControl.xaml.cs
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: AudioRecorderControl.xaml.cs
 //Version: 20121123
+
+using WPFCompatibility;
 
 using System;
 using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ClipFlair.AudioRecorder
 {
   public partial class AudioRecorderControl : UserControl
   {
-    AudioRecorderViewModel ViewModel;
-
+ 
     public AudioRecorderControl()
     {
       InitializeComponent();
+      View = new AudioRecorderView();
+    }
 
-      ViewModel = new AudioRecorderViewModel(btnRecord, btnPlay);
-
-      DataContext = ViewModel;
+    public AudioRecorderView View
+    {
+      get { return (AudioRecorderView)DataContext; }
+      set {
+        value.SetToggleButtons(btnRecord, btnPlay);
+        DataContext = value; 
+      }
     }
 
     public void Play()
     {
       //StopPlayback(); //stop any current playback //don't call this, since when clicking on Play button at other grid row we have the button checked then unchecked by this
-      ViewModel.PlayCommand.IsChecked = true; //don't talk to ToggleButton directly
+      View.PlayCommand.IsChecked = true; //don't talk to ToggleButton directly
     }
 
     public void StopPlayback()
     {
-      ViewModel.PlayCommand.IsChecked = false; //don't talk to ToggleButton directly
+      View.PlayCommand.IsChecked = false; //don't talk to ToggleButton directly
     }
-
-    public double Volume
-    {
-      get { return ViewModel.Volume; }
-      set { ViewModel.Volume = value; }
-    }
-
-    #region Load-Save
-
-    public void LoadAudio(Stream stream) //does not close the stream
-    {
-      ViewModel.LoadAudio(stream);
-    }
-
-    public void SaveAudio(Stream stream) //does not close the stream
-    {
-      ViewModel.SaveAudio(stream);
-    }
-
-    #endregion
 
   }
 
