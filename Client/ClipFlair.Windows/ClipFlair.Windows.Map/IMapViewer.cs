e606a219fc9b39b9bf8acc7a79f4c2f3150bb430
birbilis <birbilis@localhost>
2013-01-10 06:55:30 +0200

    #1238 ClipFlair: Map component showing Mode combobox at backpanel, together with LabelsVisible and LabelsFading checkboxes

--- Client/ClipFlair.Windows/ClipFlair.Windows.Map/IMapViewer.cs
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Map/IMapViewer.cs
@@ -1,6 +1,8 @@
 ﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
 //Filename: IMapViewer.cs
-//Version: 20121129
+//Version: 20130110
+
+using Microsoft.Maps.MapControl.Core;
 
 using System;
 
@@ -10,11 +12,16 @@ namespace ClipFlair.Windows.Views
   public static class IMapViewerProperties
   {
     public const string PropertyMode = "Mode";
+    public const string PropertyLabelsVisible = "LabelsVisible";
+    public const string PropertyLabelsFading = "LabelsFading";
   }
 
   public interface IMapViewer : IView
   {
+    MapMode ModeValue { get; set; }
     string Mode { get; set; }
+    bool LabelsVisible { get; set; }
+    bool LabelsFading { get; set; }
   }
 
 }
