7ef17c320c90b2b015f679cede5050213d98621b
birbilis <birbilis@localhost>
2013-01-14 17:52:46 +0200

    #1335 Map component: added MapZoom and ScaleVisible properties, hid Bing Logo and Map Copyright data (should check Bing license on this), setting default width and height (overrides from BaseView defaults) at View, not at Window anymore

--- Client/ClipFlair.Windows/ClipFlair.Windows.Map/IMapViewer.cs
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Map/IMapViewer.cs
@@ -11,21 +11,27 @@ namespace ClipFlair.Windows.Views
 
   public static class IMapViewerProperties
   {
+    public const string PropertyInertia = "Inertia";
     public const string PropertyNavigationVisible = "NavigationVisible";
+    public const string PropertyScaleVisible = "ScaleVisible";
     public const string PropertyCulture = "Culture"; //e.g. "en-us"
     public const string PropertyMode = "Mode"; //Road, Aerial
     public const string PropertyLabelsVisible = "LabelsVisible";
     public const string PropertyLabelsFading = "LabelsFading";
+    public const string PropertyMapZoom = "MapZoom";
   }
 
   public interface IMapViewer : IView
   {
+    bool Inertia { get; set; }
     bool NavigationVisible { get; set; }
+    bool ScaleVisible { get; set; }
     string Culture { get; set; }
     MapMode ModeValue { get; set; }
     string Mode { get; set; }
     bool LabelsVisible { get; set; }
     bool LabelsFading { get; set; }
+    double MapZoom { get; set; }
   }
 
 }
