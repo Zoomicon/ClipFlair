69ffca0cc8a830f29c23484698d930f4495706fc
birbilis <birbilis@localhost>
2013-01-31 18:27:39 +0200

    #1220 FloatingWindowZUI - added Zoom slider to bottom bar, binding ContentScale, ContentScalable, MinContentScale, MaxContentScale properties correctly now to hosted ZoomAndPanControl

--- Client/FloatingWindowZUI/Source/FloatingWindowHostZUI.cs
+++ Client/FloatingWindowZUI/Source/FloatingWindowHostZUI.cs
@@ -1,5 +1,5 @@
 ﻿//Filename: FloatingWindowHostZUI.cs
-//Version: 20121111
+//Version: 20130131
 
 using SilverFlow.Controls;
 using SilverFlow.Controls.Extensions;
@@ -8,6 +8,7 @@ using WPFCompatibility;
 
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 using System.Collections.Specialized;
@@ -82,6 +83,82 @@ namespace FloatingWindowZUI
       SubscribeToFloatingWindowEvents();
     }
 
+    #region ContentScalable
+
+    /// <summary>
+    /// ContentScalable Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty ContentScalableProperty =
+        DependencyProperty.Register("ContentScalable", typeof(bool), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(true));
+
+    /// <summary>
+    /// Gets or sets the ContentScalable property.
+    /// </summary>
+    public bool ContentScalable
+    {
+      get { return (bool)GetValue(ContentScalableProperty); }
+      set { SetValue(ContentScalableProperty, value); }
+    }
+
+    #endregion
+
+    #region ContentScale
+
+    /// <summary>
+    /// ContentScale Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty ContentScaleProperty =
+      WPF_DependencyProperty.Register("ContentScale", typeof(double), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(1.0)); //no need to do value Coercion, we bind to ZoomHost which will do it
+
+    /// <summary>
+    /// Gets or sets the ContentScale property.
+    /// </summary>
+    public double ContentScale
+    {
+      get { return (double)GetValue(ContentScaleProperty); }
+      set { SetValue(ContentScaleProperty, value); }
+    }
+
+    #endregion
+
+    #region MinContentScale
+
+    /// <summary>
+    /// MinContentScale Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty MinContentScaleProperty =
+      WPF_DependencyProperty.Register("MinContentScale", typeof(double), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(0.01)); //no need to do value Coercion, we bind to ZoomHost which will do it
+
+    /// <summary>
+    /// Gets or sets the MinContentScale property.
+    /// </summary>
+    public double MinContentScale
+    {
+      get { return (double)GetValue(MinContentScaleProperty); }
+      set { SetValue(MinContentScaleProperty, value); }
+    }
+
+    #endregion
+
+    #region MaxContentScale
+
+    /// <summary>
+    /// MaxContentScale Dependency Property
+    /// </summary>
+    public static readonly DependencyProperty MaxContentScaleProperty =
+      WPF_DependencyProperty.Register("MaxContentScale", typeof(double), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(10.0)); //no need to do value Coercion, we bind to ZoomHost which will do it
+
+    /// <summary>
+    /// Gets or sets the MaxContentScale property.
+    /// </summary>
+    public double MaxContentScale
+    {
+      get { return (double)GetValue(MaxContentScaleProperty); }
+      set { SetValue(MaxContentScaleProperty, value); }
+    }
+
+    #endregion
+ 
     #region FloatingWindowEvents
 
     private void SubscribeToFloatingWindowEvents()
@@ -203,248 +280,6 @@ namespace FloatingWindowZUI
     {
       ZoomHost.ZoomAboutPoint(ZoomHost.ContentScale + 0.2, contentZoomCenter);
     }
-
-    #region ZoomAndPan properties
-
-    /// <summary>
-    /// Get/set the X offset (in content coordinates) of the view on the content.
-    /// </summary>
-    public double ContentOffsetX
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ContentOffsetXProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ContentOffsetXProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Get/set the Y offset (in content coordinates) of the view on the content.
-    /// </summary>
-    public double ContentOffsetY
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ContentOffsetYProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ContentOffsetYProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Get/set the current scale (or zoom factor) of the content.
-    /// </summary>
-    public double ContentScale
-    {
-      get
-      {
-        return (double)GetValue(ContentScaleProperty);
-      }
-      set
-      {
-        SetValue(ContentScaleProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Get/set the minimum value for 'ContentScale'.
-    /// </summary>
-    public double MinContentScale
-    {
-      get
-      {
-        return (double)GetValue(MinContentScaleProperty);
-      }
-      set
-      {
-        SetValue(MinContentScaleProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Get/set the maximum value for 'ContentScale'.
-    /// </summary>
-    public double MaxContentScale
-    {
-      get
-      {
-        return (double)GetValue(MaxContentScaleProperty);
-      }
-      set
-      {
-        SetValue(MaxContentScaleProperty, value);
-      }
-    }
-
-    /*
-    /// <summary>
-    /// The X coordinate of the content focus, this is the point that we are focusing on when zooming.
-    /// </summary>
-    public double ContentZoomFocusX
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ContentZoomFocusXProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ContentZoomFocusXProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// The Y coordinate of the content focus, this is the point that we are focusing on when zooming.
-    /// </summary>
-    public double ContentZoomFocusY
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ContentZoomFocusYProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ContentZoomFocusYProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// The X coordinate of the viewport focus, this is the point in the viewport (in viewport coordinates) 
-    /// that the content focus point is locked to while zooming in.
-    /// </summary>
-    public double ViewportZoomFocusX
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ViewportZoomFocusXProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ViewportZoomFocusXProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// The Y coordinate of the viewport focus, this is the point in the viewport (in viewport coordinates) 
-    /// that the content focus point is locked to while zooming in.
-    /// </summary>
-    public double ViewportZoomFocusY
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ViewportZoomFocusYProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ViewportZoomFocusYProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// The duration of the animations (in seconds) started by calling AnimatedZoomTo and the other animation methods.
-    /// </summary>
-    public double AnimationDuration
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.AnimationDurationProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.AnimationDurationProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Get the viewport width, in content coordinates.
-    /// </summary>
-    public double ContentViewportWidth
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ContentViewportWidthProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ContentViewportWidthProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Get the viewport height, in content coordinates.
-    /// </summary>
-    public double ContentViewportHeight
-    {
-      get
-      {
-        return (double)ZoomHost.GetValue(ZoomAndPanControl.ContentViewportHeightProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.ContentViewportHeightProperty, value);
-      }
-    }
-
-    /// <summary>
-    /// Set to 'true' to enable the mouse wheel to scroll the zoom and pan control.
-    /// This is set to 'false' by default.
-    /// </summary>
-    public bool IsMouseWheelScrollingEnabled
-    {
-      get
-      {
-        return (bool)ZoomHost.GetValue(ZoomAndPanControl.IsMouseWheelScrollingEnabledProperty);
-      }
-      set
-      {
-        ZoomHost.SetValue(ZoomAndPanControl.IsMouseWheelScrollingEnabledProperty, value);
-      }
-    }
-    */
-
-    #endregion
-
-    #region ZoomAndPan DependencyProperties
-
-    public static readonly DependencyProperty ContentScaleProperty =
-            WPF_DependencyProperty.Register("ContentScale", typeof(double), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(1.0, ContentScale_PropertyChanged, ContentScale_Coerce));
-
-
-    public static readonly DependencyProperty MinContentScaleProperty =
-            WPF_DependencyProperty.Register("MinContentScale", typeof(double), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(0.01, MinOrMaxContentScale_PropertyChanged));
-
-    public static readonly DependencyProperty MaxContentScaleProperty =
-            WPF_DependencyProperty.Register("MaxContentScale", typeof(double), typeof(FloatingWindowHostZUI), new FrameworkPropertyMetadata(10.0, MinOrMaxContentScale_PropertyChanged));
-
-    /// <summary>
-    /// Event raised when the 'ContentScale' property has changed value.
-    /// </summary>
-    public static void ContentScale_PropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
-    {
-      ZoomAndPanControl.ContentScale_PropertyChanged(((FloatingWindowHostZUI)o).ZoomHost, e);
-    }
-
-    /// <summary>
-    /// Method called to clamp the 'ContentScale' value to its valid range.
-    /// </summary>
-    private static object ContentScale_Coerce(DependencyObject d, object baseValue)
-    {
-      return ZoomAndPanControl.ContentScale_Coerce(((FloatingWindowHostZUI)d).ZoomHost, baseValue);
-    }
-
-    /// <summary>
-    /// Event raised 'MinContentScale' or 'MaxContentScale' has changed.
-    /// </summary>
-    public static void MinOrMaxContentScale_PropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
-    {
-      ZoomAndPanControl.MinOrMaxContentScale_PropertyChanged(((FloatingWindowHostZUI)o).ZoomHost, e);
-    }
-
-    #endregion
 
   }
 
