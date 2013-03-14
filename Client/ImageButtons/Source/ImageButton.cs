6d8ed9d993ce15f98e333f22dd7eca52489a5277
George Birbilis <birbilis@users.noreply.github.com>
2013-03-14 18:21:42 +0200

    #1287 - ClipFlair: restyling in progress

---
+++ Client/ImageButtons/Source/ImageButton.cs
@@ -0,0 +1,52 @@
+﻿//Version: 20130314
+//based on sample from: http://loekvandenouweland.com/index.php/2011/02/silverlight-templated-image-button-with-two-images/
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ImageButtons
+{
+  public class ImageButton : Button
+  {
+
+    public ImageButton()
+    {
+      this.DefaultStyleKey = typeof(ImageButton);
+    }
+
+    #region Image
+
+    public ImageSource Image
+    {
+      get { return (ImageSource)GetValue(ImageProperty); }
+      set { SetValue(ImageProperty, value); }
+    }
+
+    public static readonly DependencyProperty ImageProperty =
+        DependencyProperty.Register("Image",
+                                    typeof(ImageSource),
+                                    typeof(ImageButton),
+                                    new PropertyMetadata(null));
+
+    #endregion
+
+    #region ImageHover
+
+    public ImageSource ImageHover
+    {
+      get { return (ImageSource)GetValue(ImageHoverProperty); }
+      set { SetValue(ImageHoverProperty, value); }
+    }
+
+    public static readonly DependencyProperty ImageHoverProperty =
+        DependencyProperty.Register("ImageHover",
+                                    typeof(ImageSource),
+                                    typeof(ImageButton),
+                                    new PropertyMetadata(null));
+
+    #endregion
+
+  }
+
+}
