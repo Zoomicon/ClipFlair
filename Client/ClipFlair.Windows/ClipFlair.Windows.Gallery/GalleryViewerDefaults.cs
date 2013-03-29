403b98609ceee783b1933cebb0f03192c7bb42c3
George Birbilis <birbilis@kagi.com>
2013-03-29 10:17:34 +0200

    #1360 - First version of ClipFlair Gallery component

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Gallery/GalleryViewerDefaults.cs
@@ -0,0 +1,44 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: GalleryViewerDefaults.cs
+//Version: 20130326
+
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ClipFlair.Windows.Views
+{
+
+  public static class GalleryViewerDefaults
+  {
+    #region IView defaults - overrides
+    
+    public const string DefaultTitle = "Gallery";
+    public const double DefaultWidth = 600;
+    public const double DefaultHeight = 400;
+        
+    #endregion
+
+    public const Uri DefaultSource = null;
+    public const Stretch DefaultStretch = Stretch.Uniform;
+
+    #region Methods
+
+    public static void SetDefaults(IGalleryViewer Gallery)
+    {
+      //IView defaults and overrides
+      ViewDefaults.SetDefaults(Gallery);
+      Gallery.Title = DefaultTitle;
+      Gallery.Width = DefaultWidth;
+      Gallery.Height = DefaultHeight;
+
+      //IGalleryViewer defaults
+      Gallery.Source = DefaultSource;
+      Gallery.Stretch = DefaultStretch;
+    }
+
+    #endregion
+
+  }
+
+}
---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Gallery/IGalleryViewer.cs
@@ -0,0 +1,23 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: IGalleryViewer.cs
+//Version: 20130326
+
+using System;
+using System.Windows.Media;
+
+namespace ClipFlair.Windows.Views
+{
+
+  public static class IGalleryViewerProperties
+  {
+    public const string PropertySource = "Source";
+    public const string PropertyStretch = "Stretch";
+  }
+
+  public interface IGalleryViewer : IView
+  {
+    Uri Source { get; set; }
+    Stretch Stretch { get; set; }
+  }
+
+}
