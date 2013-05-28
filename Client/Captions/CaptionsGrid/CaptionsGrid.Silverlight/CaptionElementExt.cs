f8d51f5af2df1a6de73a5923764015d21a329135
George Birbilis <birbilis@kagi.com>
2013-05-28 17:22:09 +0300

    #1240 - CaptionsGrid: added support for loading/storing Role implying "Role: caption" pattern for now (": " RoleSeparator), but this may be configurable in the future (probably using a non-localizable [important!] format string of the style "role: caption" or "[role] caption" etc.) - added Captions property to CaptionElementExt and using that for binding the Caption column

--- Client/Captions/CaptionsGrid/CaptionsGrid.Silverlight/CaptionElementExt.cs
+++ Client/Captions/CaptionsGrid/CaptionsGrid.Silverlight/CaptionElementExt.cs
@@ -1,6 +1,6 @@
 ﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
 //Filename: CaptionElementExt.cs
-//Version: 20130401
+//Version: 20130528
 
 using Utils.Extensions;
 
@@ -20,12 +20,18 @@ namespace ClipFlair.CaptionsGrid
   {
 
     public const string PROPERTY_DURATION = "Duration";
-    public const string PROPERTY_ACTOR = "Role";
+    public const string PROPERTY_ROLESEPARATOR = "RoleSeparator";
+    public const string PROPERTY_ROLE = "Role";
+    public const string PROPERTY_CAPTION = "Caption";
     public const string PROPERTY_WPM = "WPM";
     public const string PROPERTY_AUDIO = "Audio";
     public const string PROPERTY_COMMENTS = "Comments";
 
+    public const string DEFAULT_ROLE_SEPARATOR = ": "; //note: has a space at the end
+
+    private string _roleSeparator = DEFAULT_ROLE_SEPARATOR;
     private string _role;
+    private string _caption;
     private Stream _audio;
     private string _comments;
 
@@ -44,7 +50,24 @@ namespace ClipFlair.CaptionsGrid
     public new TimeSpan Duration //hides ancestor's "Duration" property (if it was "virtual" we'd "override" it) to add a "set" accessor method
     {
       get { return base.Duration; }
-      set { End = Begin + value; } //setting End will fire PropertyChanged event for "End" and "Duration" properties
+      set { End = Begin + value; } //setting End will fire PropertyChanged event for both "End" and "Duration" properties (and will also set the ancestor's Duration property appropriately)
+    }
+
+    /// <summary>
+    /// Gets or sets the role separator used to extract role from caption) for this marker item.
+    /// </summary>
+    [ScriptableMember]
+    public string RoleSeparator
+    {
+      get { return _roleSeparator; }
+      set
+      {
+        if (_roleSeparator != value)
+        {
+          _roleSeparator = value;
+          NotifyPropertyChanged(PROPERTY_ROLESEPARATOR);
+        }
+      }
     }
 
     /// <summary>
@@ -56,13 +79,46 @@ namespace ClipFlair.CaptionsGrid
       get { return _role; }
       set
       {
-        _role = value;
-        NotifyPropertyChanged(PROPERTY_ACTOR);
+        if (_role != value)
+        {
+          _role = value;
+          UpdateContent();
+          NotifyPropertyChanged(PROPERTY_ROLE);
+        }
+      }
+    }
+
+    [ScriptableMember]
+    protected string RoleWithSeparator
+    {
+      get
+      { 
+        string role = Role;
+        return (String.IsNullOrWhiteSpace(role))? "" : role + RoleSeparator; //never return null
       }
     }
 
     /// <summary>
-    /// Gets or sets the text associated with this marker.
+    /// Gets or sets the caption associated with this marker.
+    /// </summary>
+    [ScriptableMember]
+    public string Caption
+    {
+      get { return _caption; }
+      set
+      {
+        if (_caption != value)
+        {
+          _caption = value;
+          UpdateContent();
+          NotifyPropertyChanged(PROPERTY_CAPTION);
+          NotifyPropertyChanged(PROPERTY_WPM); //also notify that WPM changed
+        }
+      }
+    }
+
+    /// <summary>
+    /// Gets or sets the content associated with this marker.
     /// </summary>
     [ScriptableMember]
     public override object Content
@@ -73,9 +129,45 @@ namespace ClipFlair.CaptionsGrid
         if (Content != value)
         {
           base.Content = value;
-          NotifyPropertyChanged(PROPERTY_WPM); //if content changed also notify that WPM changed
+          UpdateRoleAndCaption();
         }
       }
+    }
+
+    /// <summary>
+    /// Sets Content property from Role and Caption ones
+    /// </summary>
+    private void UpdateContent()
+    {
+      object newContent = RoleWithSeparator + ((_caption != null) ? _caption : ""); //even for null captions we need to set Content to non-null since Role is persisted via the Content property
+      if (newContent != Content)
+        base.Content = newContent;
+    }
+    /// <summary>
+    /// Sets _role and _caption fields from Content property
+    /// </summary>
+    private void UpdateRoleAndCaption()
+    {
+      string content = Content as string;
+      int roleSeparatorPos = (content != null) ? content.IndexOf(RoleSeparator) : -1;
+      bool foundRoleSeparator = (roleSeparatorPos >= 0);
+      
+      string newRole = (foundRoleSeparator) ? content.Substring(0, roleSeparatorPos) : null;
+      string newCaption = (foundRoleSeparator) ? content.Substring(roleSeparatorPos + RoleSeparator.Length) : content; //if Content doesn't contain a string, then _caption will get a null value
+
+      if (_role != newRole)
+      {
+        _role = newRole;
+        NotifyPropertyChanged(PROPERTY_ROLE);
+      }
+
+      if (_caption != newCaption)
+      {
+        _caption = newCaption;
+        NotifyPropertyChanged(PROPERTY_CAPTION);
+        NotifyPropertyChanged(PROPERTY_WPM); //also notify that WPM changed
+      }
+
     }
 
     /// <summary>
@@ -85,10 +177,7 @@ namespace ClipFlair.CaptionsGrid
     public int WordCount
     {
       get
-      {
-        if (Content == null) return 0;
-        return ((string)Content).WordCount();
-      }
+      { return (_caption != null)? _caption.WordCount() : 0; }
     }
 
     /// <summary>
