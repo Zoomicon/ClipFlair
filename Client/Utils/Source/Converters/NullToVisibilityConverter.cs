7bc26b27acb7765f5ba626d2ff78a1eac64c5beb
George Birbilis <birbilis@users.noreply.github.com>
2014-01-31 16:13:34 +0200

    #1360 - Gallery: Restylled PivotViewerDefaultItemAdorner to remove fixed 16x16 image width

---
+++ Client/Utils/Source/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: NullToVisibilityConverter.cs
+//Version: 20140130
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Utils.Converters
+{
+
+  [ValueConversion(typeof(Visibility), typeof(object))]
+  public class NullToVisibilityConverter : IValueConverter
+  {
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return value == null ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return (Visibility)value == Visibility.Collapsed ? null : (object)Visibility.Visible;
+    }
+
+  }
+
+}
--- Client/Utils/Source/Converters/StringToUriConverter.cs
+++ Client/Utils/Source/Converters/StringToUriConverter.cs
@@ -1,8 +1,9 @@
 ﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
 //Filename: StringToUriConverter.cs
-//Version: 20121030
+//Version: 20140130
 
 using System;
+using System.Globalization;
 using System.Windows.Data;
 
 using Utils.Extensions;
@@ -13,13 +14,13 @@ namespace Utils.Converters
     [ValueConversion(typeof(string), typeof(Uri))]
     public class StringToUriConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string input = value as string;
             return (input!=null)? input.ToUri() : null;
         }
         
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             Uri input = value as Uri;
             if (input == null) return String.Empty;
