b51333820a1cc073ba5d2600a9552d53ab84e55c
birbilis <birbilis@localhost>
2014-03-09 14:13:59 +0200

    #1234 - Changed folder for SilverTextEditor (moved under Client/Text, since SilverNews [News Reader control] will also be there)

---
+++ Client/Text/SilverTextEditor/Source/Dialogs/PrintPreview.xaml.cs
@@ -0,0 +1,38 @@
+﻿//Filename: PrintPreview.xaml.cs
+//Version: 20130811
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace SilverTextEditor
+{
+    public partial class PrintPreview : ChildWindowExt
+    {
+        public PrintPreview()
+        {
+            InitializeComponent();
+        }
+
+        public void ShowPreview(RichTextBox rtb)
+        {   
+#if SILVERLIGHT
+            WriteableBitmap image = new WriteableBitmap(rtb, null);
+            previewImage.Source = image;            
+#else
+          //TODO (see FloatingWindow control extensions on how to get image of control on WPF)
+#endif
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
+
