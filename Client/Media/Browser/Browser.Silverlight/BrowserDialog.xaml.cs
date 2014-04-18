b52937b9db4e548baa44deca2073fc28bf5ba589
George Birbilis <birbilis@users.noreply.github.com>
2014-04-18 15:57:34 +0300

    #1384 - Added BrowserDialog and using that in OOB mode to open URLs from help button and start dialog

---
+++ Client/Media/Browser/Browser.Silverlight/BrowserDialog.xaml.cs
@@ -0,0 +1,60 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: BrowserDialog.xaml.cs
+//Version: 20140418
+
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using Utils.Extensions;
+
+namespace ClipFlair.UI.Dialogs
+{
+  public partial class BrowserDialog : ChildWindow
+  {
+    public BrowserDialog()
+    {
+      InitializeComponent();
+    }
+
+    //we inherit Title property from ancestor class
+
+    public Uri Address
+    {
+      get { return browser.Source; }
+      set {
+        try
+        {
+          browser.Source = value;
+        }
+        catch
+        {
+          //NOP
+        }
+      }
+    }
+
+    public static void Show(Uri address, string title = "", EventHandler<CancelEventArgs> closingHandler = null)
+    {
+      #if SILVERLIGHT && !WINDOWS_PHONE
+      if (!Application.Current.IsRunningOutOfBrowser) {
+        if (address != null) 
+          address.NavigateTo();
+      }
+      else
+      #endif
+      {
+        BrowserDialog prompt = new BrowserDialog();
+        prompt.Address = address;
+        prompt.Title = title;
+        if (closingHandler != null) 
+          prompt.Closing += closingHandler;
+
+        prompt.Closed += (s, e) => { prompt.Content = null; }; //remove the browser control when closed //TODO: check if this solves Reentrancy Errors when opening the browser dialog multiple times in sequence
+
+        prompt.Show();
+      }
+    }
+
+  }
+}
