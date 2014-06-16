497c284a6bdd07719dd605fd17da543e61467dec
birbilis <birbilis@localhost>
2014-06-16 21:37:50 +0300

    #1384 - BrowserDialog and PivotDialog (that one is not yet implemented fully) now show their content taking up all the OOB window

--- Client/ZUI/PivotViewerExt.Silverlight/PivotDialog.xaml.cs
+++ Client/ZUI/PivotViewerExt.Silverlight/PivotDialog.xaml.cs
@@ -1,9 +1,10 @@
 ﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
 //Filename: PivotDialog.xaml.cs
-//Version: 20140418
+//Version: 20140616
 
 using System;
 using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ClipFlair.UI.Dialogs
@@ -15,9 +16,19 @@ namespace ClipFlair.UI.Dialogs
       InitializeComponent();
     }
 
+    public override void OnApplyTemplate()
+    {
+      base.OnApplyTemplate();
+
+      //ChildWindow template part names can be found at: http://msdn.microsoft.com/en-us/library/dd833070(v=vs.95).aspx
+      FrameworkElement contentRoot = GetTemplateChild("ContentRoot") as FrameworkElement;
+
+      //make the ChildWindow content take up the whole ChildWindow area
+      contentRoot.HorizontalAlignment = HorizontalAlignment.Stretch;
+      contentRoot.VerticalAlignment = VerticalAlignment.Stretch;
+    }
+
     //we inherit Title property from ancestor class
-
-
 
     public string Input
     {
