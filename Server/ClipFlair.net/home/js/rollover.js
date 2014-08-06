0a3f6f85fb52642edda811997c6b42f91b8967a1
George Birbilis <birbilis@users.noreply.github.com>
2014-08-06 13:44:56 +0300

    #1386 - ClipFlair Home page (http://ClipFlair.net)

---
+++ Server/ClipFlair.net/home/js/rollover.js
@@ -0,0 +1,38 @@
+/*
+	Standards Compliant Rollover Script
+	Author : Daniel Nolan
+	http://www.bleedingego.co.uk/webdev.php
+*/
+
+function initRollovers() {
+	if (!document.getElementById) return
+	
+	var aPreLoad = new Array();
+	var sTempSrc;
+	var aImages = document.getElementsByTagName('img');
+
+	for (var i = 0; i < aImages.length; i++) {		
+		if (aImages[i].className == 'imgover') {
+			var src = aImages[i].getAttribute('src');
+			var ftype = src.substring(src.lastIndexOf('.'), src.length);
+			var hsrc = src.replace(ftype, '_o'+ftype);
+
+			aImages[i].setAttribute('hsrc', hsrc);
+			
+			aPreLoad[i] = new Image();
+			aPreLoad[i].src = hsrc;
+			
+			aImages[i].onmouseover = function() {
+				sTempSrc = this.getAttribute('src');
+				this.setAttribute('src', this.getAttribute('hsrc'));
+			}	
+			
+			aImages[i].onmouseout = function() {
+				if (!sTempSrc) sTempSrc = this.getAttribute('src').replace('_o'+ftype, ftype);
+				this.setAttribute('src', sTempSrc);
+			}
+		}
+	}
+}
+
+window.onload = initRollovers;
