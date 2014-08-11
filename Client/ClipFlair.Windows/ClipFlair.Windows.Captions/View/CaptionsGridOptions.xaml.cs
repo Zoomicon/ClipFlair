c8a7199624ace1c4c085e2be6a72e07d5095978d
birbilis <birbilis@localhost>
2014-08-11 04:56:28 +0300

    #1240 - CaptionsGridWindow: using separate CaptionsGridOptions component

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Captions/View/CaptionsGridOptions.xaml.cs
@@ -0,0 +1,21 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: CaptionsGridOptions.xaml.cs
+//Version: 20140811
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClipFlair.Windows.Options
+{
+
+  public partial class CaptionsGridOptions : UserControl
+  {
+
+    public CaptionsGridOptions()
+    {
+      InitializeComponent();
+    }
+
+  }
+
+}
