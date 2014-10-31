9908f50c12aff215caba3276d3a3d176be436431
birbilis <birbilis@localhost>
2014-10-31 18:13:45 +0200

    #1238 - ClipFlair Studio: - Added localization for BaseWindow - Separate BaseWindow's backpanel at BaseOptions.xaml (separate UserControl)

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Base/Silverlight/View/BaseOptions.xaml.cs
@@ -0,0 +1,54 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: BaseOptions.xaml.cs
+//Version: 20141031
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClipFlair.Windows.Options
+{
+
+  public partial class BaseOptions : UserControl
+  {
+
+    #region --- Fields ---
+
+    protected bool isTopLevel;
+
+    #endregion
+
+    #region --- Initialization ---
+
+    public BaseOptions()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+    #region --- Properties ---
+
+    public bool IsTopLevel
+    {
+      get { return isTopLevel; }
+      set
+      {
+        if (isTopLevel != value)
+          isTopLevel = value;
+
+        Visibility visibility = value ? Visibility.Collapsed : Visibility.Visible; //hide backpanel properties not relevant when not being a child window
+        propX.Visibility = visibility;
+        propY.Visibility = visibility;
+        propWidth.Visibility = visibility;
+        propHeight.Visibility = visibility;
+        propZoom.Visibility = visibility;
+        propMoveable.Visibility = visibility;
+        propResizable.Visibility = visibility;
+        propZoomable.Visibility = visibility;
+      }
+    }
+
+    #endregion
+  }
+
+}
