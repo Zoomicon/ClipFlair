afc0c5dbd987b358c03d3b5af64464968cc23ddb
birbilis <birbilis@localhost>
2014-10-31 20:47:27 +0200

    #1335 - Mapcomponent: - separated options UI to separate TextEditorOptions.xaml (UserControl) file                       - added reference to FloatingWindow.Silverlight

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Map/View/MapOptions.xaml.cs
@@ -0,0 +1,21 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: MapOptions.xaml.cs
+//Version: 20141031
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClipFlair.Windows.Options
+{
+
+  public partial class MapOptions : UserControl
+  {
+
+    public MapOptions()
+    {
+      InitializeComponent();
+    }
+
+  }
+
+}
