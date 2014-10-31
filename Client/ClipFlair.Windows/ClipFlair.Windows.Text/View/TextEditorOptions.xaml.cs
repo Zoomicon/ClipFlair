384762a9b05663f8949cc939d7a6f67c4af156a6
birbilis <birbilis@localhost>
2014-10-31 20:15:39 +0200

    #1234 - Text component: separated options UI to separate TextEditorOptions.xaml (UserControl) file

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Text/View/TextEditorOptions.xaml.cs
@@ -0,0 +1,21 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: TextEditorOptions.xaml.cs
+//Version: 20141031
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClipFlair.Windows.Options
+{
+
+  public partial class TextEditorOptions : UserControl
+  {
+
+    public TextEditorOptions()
+    {
+      InitializeComponent();
+    }
+
+  }
+
+}
