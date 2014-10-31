00a13dd33633c433246af072e438e8f3b69db8b8
George Birbilis <birbilis@users.noreply.github.com>
2014-10-31 21:14:45 +0200

    #1238 - ActivityWindow (Component): separated options UI to separate ActivityOptions.xaml (UserControl) file

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Activity/View/ActivityOptions.xaml.cs
@@ -0,0 +1,21 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: ActivityOptions.xaml.cs
+//Version: 20140811
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClipFlair.Windows.Options
+{
+
+  public partial class ActivityOptions : UserControl
+  {
+
+    public ActivityOptions()
+    {
+      InitializeComponent();
+    }
+
+  }
+
+}
