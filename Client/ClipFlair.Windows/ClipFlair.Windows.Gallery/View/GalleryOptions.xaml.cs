63076ed909c61f57389a95658638af49eb869e2b
birbilis <birbilis@localhost>
2014-10-31 21:02:00 +0200

    #1360 - Gallery: separated options UI to separate GalleryOptions.xaml (UserControl) file

---
+++ Client/ClipFlair.Windows/ClipFlair.Windows.Gallery/View/GalleryOptions.xaml.cs
@@ -0,0 +1,21 @@
+﻿//Project: ClipFlair (http://ClipFlair.codeplex.com)
+//Filename: GalleryOptions.xaml.cs
+//Version: 20141031
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClipFlair.Windows.Options
+{
+
+  public partial class GalleryOptions : UserControl
+  {
+
+    public GalleryOptions()
+    {
+      InitializeComponent();
+    }
+
+  }
+
+}
