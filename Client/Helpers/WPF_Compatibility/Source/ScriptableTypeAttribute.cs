f483280be195eb96f1ee147ef2252e69ae350bcc
George Birbilis <birbilis@users.noreply.github.com>
2015-02-04 16:43:34 +0200

    Added portable Label/BusyIndicator/Accordion-AccordionItem classes

---
+++ Client/Helpers/WPF_Compatibility/Source/ScriptableTypeAttribute.cs
@@ -0,0 +1,23 @@
+﻿//Filename: ScriptableType.cs
+//Version: 20150204
+
+#if !SILVERLIGHT
+
+namespace System.Windows.Browser
+{
+
+  [AttributeUsageAttribute(AttributeTargets.Class | AttributeTargets.Struct, 
+                           AllowMultiple = false, 
+                           Inherited = false)]
+  public sealed class ScriptableTypeAttribute : Attribute
+  {
+
+    public ScriptableTypeAttribute()
+    {
+    }
+
+  }
+
+}
+
+#endif
